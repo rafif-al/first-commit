416a8a206d1d6c4a019e0ce7438657f9c2d91d2e
rafif-al <rafifalfauan@gmail.com>
2025-09-26 14:22:29 +0700

    belajar for loop

---
+++ 8_operator.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  /**
+   * numberOne penulisan dengan camel case
+   * number_one penulisan dengan snake case
+   */
+
+  var numberOne = 10;
+  var numberTwo = 20;
+
+  print("$numberOne + $numberTwo = ${numberOne + numberTwo}");
+}
