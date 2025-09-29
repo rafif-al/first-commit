87ac352101ef8bcc2c713b9342ada97d0e358821
rafif-al <rafifalfauan@gmail.com>
2025-09-29 14:16:17 +0700

    ok

---
+++ 17_while_loop.dart
@@ -0,0 +1,20 @@
+main(List<String> args) {
+  /**
+   * perulangan while di gunakan menjalakan ckode blok selama kondisi bernilai true
+   * cocok digunakan untuk perulangan dengan jumlah yang tidak diketahui SECARA PASTI
+   * penggunaannya di awali dengan kondisi while(kondisi){output}
+  */  
+
+  var i = 1;
+  while (i <= 10) {
+    print('ini angka ke $i');
+    i++;
+  }
+
+  var angka = 10;
+  while (angka >= 0) {
+    print('ini angka ke $angka');
+    angka--;
+  }
+}
+
